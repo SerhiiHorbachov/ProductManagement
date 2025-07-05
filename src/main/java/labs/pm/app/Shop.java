d3880caa06b1dc91dc42acbc4fbf3252b841c588
Serhii H <srg.gorbachov@gmail.com>
2025-07-05 19:46:38 +0300

    init. practice 4-2: documentation

--- src/main/java/labs/pm/app/Shop.java
+++ src/main/java/labs/pm/app/Shop.java
@@ -4,6 +4,13 @@ import labs.pm.data.Product;
 
 import java.math.BigDecimal;
 
+
+/**
+ * {@code Shop} class represents an application that manages Products
+ *
+ * @author oracle
+ * @version 4.0
+ */
 public class Shop {
     public static void main(String[] args) {
         Product p1 = new Product();
@@ -11,6 +18,6 @@ public class Shop {
         p1.setName("Test");
         p1.setPrice(BigDecimal.valueOf(1.99));
 
-        System.out.println(p1.getId() + " " + p1.getName() + " " + p1.getPrice());
+        System.out.println(p1.getId() + " " + p1.getName() + " " + p1.getPrice() + " " + p1.getDiscount());
     }
 }
--- src/main/java/labs/pm/data/Product.java
+++ src/main/java/labs/pm/data/Product.java
@@ -1,8 +1,29 @@
 package labs.pm.data;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
+
+/**
+ * {@code Product} class represent properties and behaviors of product objects in the Product Management System.
+ * <br>
+ * Each product has an id, name and price
+ * <br>
+ * Each product can have a discount, calculated based on a
+ * {@link DISCOUNT_RATE discount rate}
+ *
+ * @author oracle
+ * @version 4.0
+ */
 public class Product {
+
+    /**
+     * A constant that defines a
+     * {@link java.math.BigDecimal BigDecimal} value of the discount rate
+     * <br>
+     * Discount rate is 10%
+     */
+    private static final BigDecimal DISCOUNT_RATE = BigDecimal.valueOf(0.1);
 
     private int id;
     private String name;
@@ -12,7 +33,7 @@ public class Product {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(final int id) {
         this.id = id;
     }
 
@@ -20,7 +41,7 @@ public class Product {
         return name;
     }
 
-    public void setName(String name) {
+    public void setName(final String name) {
         this.name = name;
     }
 
@@ -28,7 +49,18 @@ public class Product {
         return price;
     }
 
-    public void setPrice(BigDecimal price) {
+    public void setPrice(final BigDecimal price) {
         this.price = price;
+    }
+
+    /**
+     * Calculates discount based on a product price and
+     * {@link DISCOUNT_RATE discount rate}
+     *
+     * @return a {@link java.math.BigDecimal BigDecimal}
+     * value of the discount
+     */
+    public BigDecimal getDiscount() {
+        return price.multiply(DISCOUNT_RATE).setScale(2, RoundingMode.HALF_UP);
     }
 }
