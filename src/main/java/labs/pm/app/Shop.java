f446e59b0fff3b45da3ec09cfb60ccac88535b81
Serhii H <email.com>
2025-07-14 17:02:03 +0300

    practice 10-2: Produce customized Product Reports

--- src/main/java/labs/pm/app/Shop.java
+++ src/main/java/labs/pm/app/Shop.java
@@ -6,6 +6,7 @@ import labs.pm.data.Rating;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.Comparator;
 
 
 /**
@@ -26,37 +27,49 @@ public class Shop {
         p1 = pm.reviewProduct(101, Rating.FOUR_STAR, "Good tea");
         p1 = pm.reviewProduct(101, Rating.FIVE_STAR, "Perfect tea");
         p1 = pm.reviewProduct(101, Rating.THREE_STAR, "Just add some lemon");
-        pm.printProductReport(101);
+//        pm.printProductReport(101);
 
-        pm.changeLocale("en-GB");
+//        pm.changeLocale("en-GB");
 
         Product p2 = pm.createProduct(102, "Coffee", BigDecimal.valueOf(1.99), Rating.NOT_RATED);
         p2 = pm.reviewProduct(102, Rating.THREE_STAR, "Coffee was ok");
         p2 = pm.reviewProduct(102, Rating.ONE_STAR, "Where is the milk");
         p2 = pm.reviewProduct(102, Rating.FIVE_STAR, "It's perfect with ten spoons of sugar!");
-        pm.printProductReport(102);
+//        pm.printProductReport(102);
 
         Product p3 = pm.createProduct(103, "Cake", BigDecimal.valueOf(1.99), Rating.NOT_RATED, LocalDate.now().plusDays(2));
         p3 = pm.reviewProduct(103, Rating.FIVE_STAR, "Very nice cake");
         p3 = pm.reviewProduct(103, Rating.FOUR_STAR, "It's good but I've expected more chocolate");
         p3 = pm.reviewProduct(103, Rating.FIVE_STAR, "This cake is perfect!");
-        pm.printProductReport(103);
+//        pm.printProductReport(103);
 
         Product p4 = pm.createProduct(104, "Cookie", BigDecimal.valueOf(2.99), Rating.NOT_RATED, LocalDate.now());
         p4 = pm.reviewProduct(104, Rating.THREE_STAR, "Just another cookie");
         p4 = pm.reviewProduct(104, Rating.THREE_STAR, "OK");
-        pm.printProductReport(104);
+//        pm.printProductReport(104);
 
         Product p5 = pm.createProduct(105, "Hot Chocolate", BigDecimal.valueOf(2.50), Rating.NOT_RATED);
         p5 = pm.reviewProduct(105, Rating.FOUR_STAR, "Tasty");
         p5 = pm.reviewProduct(105, Rating.FOUR_STAR, "Not bad at all");
-        pm.printProductReport(105);
+//        pm.printProductReport(105);
 
         Product p6 = pm.createProduct(106, "Chocolate", BigDecimal.valueOf(2.50), Rating.NOT_RATED, LocalDate.now().plusDays(3));
         p6 = pm.reviewProduct(106, Rating.TWO_STAR, "Too sweet");
         p6 = pm.reviewProduct(106, Rating.THREE_STAR, "Better than cookie");
         p6 = pm.reviewProduct(106, Rating.TWO_STAR, "Too bitter");
         p6 = pm.reviewProduct(106, Rating.ONE_STAR, "I don't get it");
-        pm.printProductReport(106);
+//        pm.printProductReport(106);
+
+        pm.printProducts((prod1, prod2) -> prod1.getRating().ordinal() - prod2.getRating().ordinal());
+        pm.printProducts((prod1, prod2) -> prod2.getPrice().compareTo(prod1.getPrice()));
+        pm.printProducts(Comparator.comparingInt((Product prod) -> prod.getName().length()).reversed());
+
+        Comparator<Product> ratingSorter = (prod1, prod2) ->
+                prod2.getRating().ordinal() - prod1.getRating().ordinal();
+
+        Comparator<Product> priceSorter = (prod1, prod2) ->
+                prod2.getPrice().compareTo(prod1.getPrice());
+
+        pm.printProducts(priceSorter.thenComparing(ratingSorter).reversed());
     }
 }
--- src/main/java/labs/pm/data/ProductManager.java
+++ src/main/java/labs/pm/data/ProductManager.java
@@ -8,6 +8,7 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
@@ -111,6 +112,19 @@ public class ProductManager {
 
         if (reviews.isEmpty()) {
             txt.append(formatter.getText("no.review"));
+            txt.append("\n");
+        }
+
+        System.out.println(txt);
+    }
+
+    public void printProducts(Comparator<Product> sorter) {
+        List<Product> productList = new ArrayList<>(products.keySet());
+        productList.sort(sorter);
+
+        StringBuilder txt = new StringBuilder();
+        for (Product product : productList) {
+            txt.append(formatter.formatProduct(product));
             txt.append("\n");
         }
 
